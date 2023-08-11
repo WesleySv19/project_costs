646a7e17eb29a8015b5836070ddec83e01032345
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-08-10 21:52:03 -0300

    organizing routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+          <Link to='/'>Home</Link>
+          <Link to='/contact'>Contato</Link>
+          <Link to='/company'>Empresa</Link>
+          <Link to='/newProject'>Novo projeto</Link>
+      </div> 
+
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/newProject" element={<NewProject />} />
+      </Routes>
+      <p>Footer</p>     
+    </Router>
   );
 }
 
