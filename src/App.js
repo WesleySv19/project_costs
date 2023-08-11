ecb6d7dce015a7955beb8e2340f3371371d3d900
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-08-10 22:32:44 -0300

    add container

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
+
     <Router>
       <div>
-          <Link to='/'>Home</Link>
-          <Link to='/contact'>Contato</Link>
-          <Link to='/company'>Empresa</Link>
-          <Link to='/newProject'>Novo projeto</Link>
-      </div> 
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newProject'>Novo projeto</Link>
+      </div>
 
-      <Routes>
+      <Container customClass='min-height'>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/company" element={<Company />} />
           <Route path="/newProject" element={<NewProject />} />
-      </Routes>
-      <p>Footer</p>     
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
     </Router>
   );
 }
