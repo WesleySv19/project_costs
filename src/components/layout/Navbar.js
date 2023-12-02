32bba2eb11c5b6eb946b1a500cb2c27bce71f60d
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-02 17:33:28 -0300

    finishing navbar component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,16 +2,35 @@ import { Link } from "react-router-dom"
 import Container from './Container'
 import styles from './Navbar.module.css'
 import Logo from '../../assets/costs_logo.png'
+
 const Navbar = () => {
   return (
-    <nav>
-        <Container>
-            <Link to="/"><img src={Logo} alt="Costs" /></Link>
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={Logo} alt="Costs" />
+        </Link>
+
+        <ul className={styles.list}>
+
+          <li className={styles.item}>
             <Link to="/">Home</Link>
+          </li>
+
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+
+          <li className={styles.item}>
             <Link to="contact">Contato</Link>
+          </li>
+
+          <li className={styles.item}>
             <Link to="company">Empresa</Link>
-            <Link to="newProject">Novo Projeto</Link>
-        </Container>
+          </li>
+
+        </ul>
+      </Container>
     </nav>
   )
 }
