fedf2ba41c980804d30582b9c87ac30e113e6753
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-08 23:31:53 -0300

    improving function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Projects from "./components/pages/Projects";
 
-function App() {
+const App = () => {
   return (
 
     <Router>
