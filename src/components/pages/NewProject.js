287abb473987bd1d59f0e80c8c4c306c4dde1ace
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-09 02:04:10 -0300

    improving component

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,12 @@
-function NewProject() {
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
+
+const NewProject = () => {
     return (
-        <div>
+        <div className={styles.newProject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <p>Formuláriogit </p>
+            <ProjectForm />
         </div>
     )
 }
