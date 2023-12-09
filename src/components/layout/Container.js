92eee81f40e9ce22029eb8b52e4dacee520f3aaa
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-08 23:29:27 -0300

    improving function

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,6 +1,6 @@
 import styles from './Container.module.css'
 
-function Container(props) {
+const Container = (props) => {
     return <div className={`${styles.container} ${styles[props.customClass]}` }>{props.children}</div>
 }
 
