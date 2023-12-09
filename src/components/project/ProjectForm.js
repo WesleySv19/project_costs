f03c899082a50cfbe6166f86a1686dce551a7c98
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-09 02:07:05 -0300

    creating projectForm component

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,28 @@
+
+
+const ProjectForm = () => {
+    return (
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projeto" />
+            </div>
+
+            <div>
+                <input type="number" placeholder="Insira o orçamento" />
+            </div>
+
+            <div>
+                <select name="category_id" id="">
+                    <option disabled selected>Selecione a categoria</option>
+                </select>
+            </div>
+
+            <div>
+                <input type="submit" value="Criar projeto"/>
+            </div>
+
+        </form>
+    )
+}
+
+export default ProjectForm
