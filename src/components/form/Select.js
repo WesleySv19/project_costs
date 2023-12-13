b12d2763d478a76f77f0b595f49d066aaa7fd171
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-12 23:19:32 -0300

    inserting dynamic categories

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,7 +5,13 @@ const Select = ({ text, name, options, handleOnchange, value }) => {
     <div className={styles.form_control}>
         <label htmlFor={name}>{text}:</label>
         <select name={name} id={name}>
+
             <option>Selecione uma opção</option>
+            {options.map((option) => (
+              <option value={option.id} key={option.id}>
+                {option.name}
+              </option>
+            ))}
         </select>
     </div>
   )
