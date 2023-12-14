2fb36564cbb6c4bf00c1dfe973486235120b2870
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-13 23:51:43 -0300

    adding the requisition to projects

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,33 @@
 import styles from './NewProject.module.css'
 import ProjectForm from '../project/ProjectForm'
+import { useNavigate } from 'react-router-dom'
 
 const NewProject = () => {
+
+    const navigate = useNavigate
+
+    const createPost = (project) => {
+        const url = "http://localhost:5000/projects"
+        project.cost = 0
+        project.services = []
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(project)
+        }).then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+            }).catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.newProject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
         </div>
     )
 }
