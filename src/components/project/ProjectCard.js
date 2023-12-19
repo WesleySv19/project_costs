0062495caf1e2ea784fe29817d80a6073ecb644e
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-18 23:21:15 -0300

    creating new component

---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,10 @@
+import styles from './ProjectCard.module.css'
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+const ProjectCard = ({ id, name, budget, category, handleRemove }) => {
+  return (
+    <div>ProjectCard</div>
+  )
+}
+
+export default ProjectCard
