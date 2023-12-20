04a457db558d38bf578de86fcaa9128865116e64
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-19 22:49:53 -0300

    creating new component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import styles from './Projects.module.css'
 import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const Projects = () => {
+
+  const [projects, setProjects] = useState([])
 
   const location = useLocation()
     let message = ''
@@ -12,10 +18,29 @@ const Projects = () => {
         message = location.state.message
     }
 
+    useEffect(() => {
+      const url = "http://localhost:5000/projects"
+
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json"
+        },
+      }).then()
+    }, [])
+
   return (
-    <div>
-      <h1>Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newProject" text="Criar projetos"/>
+      </div>
       {message && <Message type="success" msg={message}/>}
+
+      <Container customClass="start">
+        <p>Projetos...</p>
+
+      </Container>
     </div>
   )
 }
